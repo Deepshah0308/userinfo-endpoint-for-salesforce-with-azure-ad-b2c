a023a709a25182e6b5cb4c4c6678f2ca73f7c1ea
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-05-24 13:59:17 +0200

    Move to express

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-var http = require('http');
+const express = require("express");
+const {json} = require("body-parser");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({
-	  "sub": "00uid4BxXw6I6TV4m0g3",
-	  "name" :"John Doe",
-	  "nickname":"Jimmy",
-	  "given_name":"John",
-	  "middle_name":"James",
-	  "family_name":"Doe",
-	  "profile":"https://example.com/john.doe",
-	  "zoneinfo":"America/Los_Angeles",
-	  "locale":"en-US",
-	  "updated_at":1311280970,
-	  "email":"mheisterberg+johndoe@salesforce.com",
-	  "email_verified":true,
-	  "address" : { "street_address":"123 Hollywood Blvd.", "locality":"Los Angeles", "region":"CA", "postal_code":"90210", "country":"US" },
-	  "phone_number":"+1 (425) 555-1212"
-	}));
-}).listen(process.env.PORT || 3000);
+const app = express();
+app.use(json());
+
+app.use(async (req, res, next) => {
+	console.log(req.originalUrl);
+	console.log(req.body);
+
+	res.type("application/json");
+	res.status(200).send(JSON.stringify({
+      sub: "00uid4BxXw6I6TV4m0g3",
+      name: "John Doe",
+      nickname: "Jimmy",
+      given_name: "John",
+      middle_name: "James",
+      family_name: "Doe",
+      profile: "https://example.com/john.doe",
+      zoneinfo: "America/Los_Angeles",
+      locale: "en-US",
+      updated_at: 1311280970,
+      email: "mheisterberg+johndoe@salesforce.com",
+      email_verified: true,
+      address: {
+        street_address: "123 Hollywood Blvd.",
+        locality: "Los Angeles",
+        region: "CA",
+        postal_code: "90210",
+        country: "US",
+      },
+      phone_number: "+1 (425) 555-1212",
+    })
+  ).end();
+})
+app.listen(process.env.PORT || 3000);
