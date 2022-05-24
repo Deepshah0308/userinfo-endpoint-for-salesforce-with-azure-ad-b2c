ed2b1b5150ead384ebb9a5ef320137a16e833bb5
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-05-24 14:19:31 +0200

    Minimize response

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 const express = require("express");
-const {json} = require("body-parser");
+const { json } = require("body-parser");
 
 const app = express();
 app.use(json());
 
 app.use(async (req, res, next) => {
-	console.log(req.originalUrl);
-	Object.keys(req.headers).forEach(key => {
-		console.log(`${key} = ${req.headers[key]}`);
-	})
-	console.log(req.body);
+    const auth_header = req.headers.authorization;
+    if (!auth_header) return res.status(401).send("Unauthenticated").end();
+    const claims = JSON.parse(Buffer.from(auth_header.split(".")[1], "base64").toString());
+    const sub = claims.sub;
+    const aud = claims.aud;
+    if (aud !== (process.env.OIDC_AUDIENCE || "44fd6d29-faad-475f-ad06-855b554d9353")) {
+		return res.status(401).send("Invalid audience");
+	}
 
+	// create response body
+	const response_body = {
+        sub /*,
+                name: "John Doe",
+                nickname: "Jimmy",
+                given_name: "John",
+                middle_name: "James",
+                family_name: "Doe",
+                profile: "https://example.com/john.doe",
+                zoneinfo: "America/Los_Angeles",
+                locale: "en-US",
+                updated_at: 1311280970,
+                email: "mheisterberg+johndoe@salesforce.com",
+                email_verified: true,
+                address: {
+                    street_address: "123 Hollywood Blvd.",
+                    locality: "Los Angeles",
+                    region: "CA",
+                    postal_code: "90210",
+                    country: "US",
+                },
+                phone_number: "+1 (425) 555-1212",*/,
+    };
+	console.log(response_body);
+    
+	// send response
 	res.type("application/json");
-	res.status(200).send(JSON.stringify({
-      sub: "00uid4BxXw6I6TV4m0g3",
-      name: "John Doe",
-      nickname: "Jimmy",
-      given_name: "John",
-      middle_name: "James",
-      family_name: "Doe",
-      profile: "https://example.com/john.doe",
-      zoneinfo: "America/Los_Angeles",
-      locale: "en-US",
-      updated_at: 1311280970,
-      email: "mheisterberg+johndoe@salesforce.com",
-      email_verified: true,
-      address: {
-        street_address: "123 Hollywood Blvd.",
-        locality: "Los Angeles",
-        region: "CA",
-        postal_code: "90210",
-        country: "US",
-      },
-      phone_number: "+1 (425) 555-1212",
-    })
-  ).end();
-})
+    res.status(200)
+        .send(
+            JSON.stringify(response_body)
+        )
+        .end();
+});
 app.listen(process.env.PORT || 3000);
